fc33c235c61b089a69563f4e3e3161563525f87a
Ethan <EthanAEdwards5@gmail.com>
2023-07-01 14:59:25 -0500

    Starter

---
+++ web/main.js
@@ -0,0 +1,16 @@
+//Directory Input
+ const dirPath = document.getElementById("dirInput");
+
+
+
+//Onclick
+document.querySelector("button").onclick = function () {
+	//Call python funciton
+	eel.synthesize_activate()(function (text) {
+		//update div with message
+		document.querySelector(".synthesize_activate").innerHTML = text;
+	});
+
+	//hides button afterwards
+	document.querySelector("button").style.display = "none";
+};
